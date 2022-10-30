d4bb887cebb89e3e16f120d753a58fe0910a78f4
JoshQC <joshua.quesada@uned.cr>
2022-10-30 01:13:25 -0600

    Refactorizar la plantilla de proyectos y agregar los estilos a la tarjeta y formulario

---
+++ SistemaDeReporteria.AplicacionWeb/Models/VariablesProyecto.cs
@@ -0,0 +1,23 @@
+﻿namespace SistemaDeReporteria.AplicacionWeb.Models
+{
+    public class VariablesProyecto
+    {
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Estado { get; set; }
+        public string Tipo { get; set; }
+        public string Area_Cientifica { get; set; }
+        public string Objetivo_Socioeconomico { get; set; }
+        public string Eje_De_Planes { get; set; }
+        public string Unidad_De_Investigacion { get; set; }
+        public DateTime Fecha_De_Inicio { get; set; }
+        public DateTime Fecha_De_Finalizacion { get; set; }
+        public string Canton { get; set; }
+        public string Region { get; set; }
+        public string Objetivo_De_Desarrollo_Sostenible { get; set; }
+        public string Grado_Academico { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+
+    }
+}
