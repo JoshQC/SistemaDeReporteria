edd2e5b06089be4d4b93c87ab011ded9f1ba4ba5
JoshQC <joshua.quesada@uned.cr>
2023-03-06 23:46:45 -0600

    Limitar la cantidad de checkbox accesibles a 4 y manejar el codigo js desde el archivo ValidacionesYEventos

---
+++ SistemaDeReporteria.AplicacionWeb/wwwroot/js/ValidacionesYEventos.js
@@ -0,0 +1,41 @@
+﻿const agregarEventoToggle = (variables) => {
+    variables.forEach((variable) => {
+        const ck = document.getElementById(`ck_${variable}`);
+        const contenedor = document.getElementById(`contenedor_${variable}`);
+        ck.addEventListener('change', () => {
+            contenedor.classList.toggle("show");
+        })
+    });
+}
+
+const validarCantidadCheckboxActivos = () => {
+    let checkboxes = document.querySelectorAll('.contenedor-check-variable input[type="checkbox"]');
+    let cantidadCheckboxesActivos = 0;
+    const MAX_CANTIDAD_CHECKBOX = 4;
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('click', function () {
+            if (this.checked) {
+                cantidadCheckboxesActivos++;
+
+                if (cantidadCheckboxesActivos == MAX_CANTIDAD_CHECKBOX) {
+                    for (var j = 0; j < checkboxes.length; j++) {
+                        if (!checkboxes[j].checked) {
+                            checkboxes[j].disabled = true;
+                        }
+                    }
+                }
+            } else {
+                cantidadCheckboxesActivos--;
+
+                if (cantidadCheckboxesActivos == MAX_CANTIDAD_CHECKBOX - 1) {
+                    for (var j = 0; j < checkboxes.length; j++) {
+                        if (!checkboxes[j].checked) {
+                            checkboxes[j].disabled = false;
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
